d1ce9a37fe9150e0e0495c693f4ec0200ef59941
lucasgaertner <bartner@msn.com>
2020-09-15 17:16:35 +0200

    added flexible min_edit Distance table

--- completion/gui/main.py
+++ completion/gui/main.py
@@ -7,7 +7,7 @@ import os
 import re
 
 #from auto.main import *
-from constant import BLOCKSIZE, BLUE, GREY
+from constant import BLOCKSIZE, BLUE, GREY, GREEN, WHITE
 from auto.processing import process_data, get_count, get_probs
 from auto.min_edit import min_edit_distance
 from auto.corrections import get_corrections
@@ -30,26 +30,22 @@ def main():
 
 
 
-    source =  'play'
-    target = 'stay'
-    matrix, min_edits, tmp_matrix = min_edit_distance(source, target)
-    idx = list('#' + source)
-    cols = list('#' + target)
-    df = pd.DataFrame(matrix, index=idx, columns= cols)
-    row, col = df.shape[0], df.shape[1]
-    win_width = row * BLOCKSIZE + 300
-    win_height = col * BLOCKSIZE 
+    win_width = 550 
+    win_height = 500
 
+    row = 2
+    col = 2
 
     LIMIT = 0
     DONE = False
     position_dict = {}
     board = Board()
     SCREEN, CLOCK = board.Grid(win_height, win_width)
-    grid = board.Matrix(row, col)
-
+    
+    font = pygame.font.Font('freesansbold.ttf', 32)
     while not DONE:      
         events = pygame.event.get()
+        edits = False
         for event in events: 
             if event.type == pygame.QUIT:
                 DONE = True
@@ -60,23 +56,40 @@ def main():
                 word = re.findall('\w+', words)
                 l = len(word) - 1
                 word = word[l]
+                print("this is the last typed word:", word)
                 n_best = get_corrections(word, probs, vocab, n=2, verbose=False)
-                print(n_best)
+                print("nbest solutions:", n_best)
                 if n_best != [] and len(n_best[0]) > 1:
+                    edits = True
+                    print("Got a n_best Solution", n_best)
                     source = word
-                    target = n_best[0][0]
-                    print()
-                    #pygame.draw.rect(SCREEN,GREY,(10, win_height))
+                    # recommendation over text ares
+                    solutions = len(n_best[0])
+                    pygame.draw.rect(SCREEN,GREEN,(10,win_width/2,win_height-20,50))
+                    for idx, n in enumerate(n_best):
+                        set_rect = (win_height/solutions*idx + 10,win_width/2,win_height-20, 50)
+                        text = font.render(str(n[0]), True, WHITE)
+                        SCREEN.blit(text, set_rect)
+                        target = n_best[0][0]
+                    matrix, min_edits, tmp_matrix = min_edit_distance(source, target)
+                    idx = list('#' + source)
+                    cols = list('#' + target)
+                    df = pd.DataFrame(matrix, index=idx, columns= cols)
+                    row, col = df.shape[0], df.shape[1]
+                    grid = board.Matrix(row, col)
                 else:
-                    pass
+                    edits = False
+                    print("No Solution Matrix")
                 board.draw_squares(SCREEN, CLOCK, row, col, grid)
-                board.set_source(SCREEN, CLOCK, row, col, df, source, target)
-                pygame.display.update()
-                board.fill(SCREEN, CLOCK, row, col, tmp_matrix, source, target)
-        pygame.draw.rect(SCREEN,BLUE,(0,win_height+100,win_width,win_height + 200))
+                if edits:
+                    board.set_source(SCREEN, CLOCK, row, col, df, source, target)
+                    pygame.display.update()
+                    board.fill(SCREEN, CLOCK, row, col, tmp_matrix, source, target)
+
+        pygame.draw.rect(SCREEN,BLUE,(0,win_width/2 + 50,win_width,win_width/3 + 50)) # Plus 50 because of recommendation
         text = textinput.update(events)
         # Blit its surface onto the screen
-        SCREEN.blit(textinput.get_surface(), (10, win_height+100))
+        SCREEN.blit(textinput.get_surface(), (10, win_width/2 + 50))
         pygame.display.update()
         CLOCK.tick(30)
 
